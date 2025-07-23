269021bfc9eef10531175c3edab4464e281e2720
Your Name <you@example.com>
2025-07-23 16:56:03 +0530

    on master

--- server-config.sh
+++ server-config.sh
@@ -1,29 +1,19 @@
 #!/bin/bash
+
 set -e
 
-echo "Installing Java 17 via Amazon Corretto..."
+sudo yum install -y java-1.8.0-openjdk-devel
+sudo yum install -y git
+sudo yum install -y maven
 
-# Add Corretto 17 repo and install
-sudo rpm --import https://yum.corretto.aws/corretto.key
-sudo curl -L -o /etc/yum.repos.d/corretto.repo https://yum.corretto.aws/corretto.repo
-sudo dnf install -y java-17-amazon-corretto-devel
 
-# Verify installation
-java -version
-
-echo "Installing Git and Maven..."
-sudo dnf install -y git maven
-
-# Clone or update the repo
-if [ -d "/home/ec2-user/addressbook1" ]; then
-    echo "Repo exists. Pulling latest changes..."
-    cd /home/ec2-user/addressbook1
-    git pull origin master
+if [ -d "addressbook1" ]
+then
+echo "repo is cloned and repo is exits"
+cd /home/ec2-user/addressbook1
+git pull origin master
 else
-    echo "Cloning repo..."
-    git clone https://github.com/Deepthirao192/addressbook1.git /home/ec2-user/addressbook1
-    cd /home/ec2-user/addressbook1
+git clone https://github.com/Deepthirao192/addressbook1.git
 fi
-
-echo "Packaging the project..."
-mvn clean package
+cd /home/ec2-user/addressbook1
+mvn package
